9c8e4305c287915e0cdc8f442eddbeda8c37adec
Aleksandr1UA <uatehno@ukr.net>
2022-08-01 20:05:10 +0300

    first commit

--- app.js
+++ app.js
@@ -1,61 +1,41 @@
 let rates = [];
 
-currentDate = (date) => {
-    return [date.getFullYear(), date.getMonth(), date.getDate()].join(0,''); 
-}
-
 renderRates = (rates) => {
     const htmlStr = rates.map((rate) =>
         `<tr>
-            <td>${rate.r030}</td>
-            <td>${rate.txt}</td>
+            <td>${rate.name}</td>
             <td>${rate.rate}</td>
             <td>${rate.cc}</td>
-            <td>${rate.exchangedate}</td>
         </tr>`).join('');
-    document.querySelector('.rates tbody').innerHTML = htmlStr || `<tr><td colspan="5" class="text-center">Not Found</td></tr>`;
+    document.querySelector('.rates tbody').innerHTML = htmlStr || `<tr><td colspan="3" class="text-center">Not Found</td></tr>`;
 }
 
 document.querySelector('#search').onkeyup = (e) => {
     let value = e.currentTarget.value.toLowerCase().trim();
-    let filteredRates = rates.filter(rate => rate.txt.toLowerCase().includes(value));
+    let filteredRates = rates.filter(rate => rate.name.toLowerCase().includes(value));
     
     renderRates(filteredRates);
 }
 
-document.getElementById('date').onchange = (e) => {
-    const selectDate = e.currentTarget.value.replaceAll('-', '');
-    fetch(`https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=${selectDate}&json`)
+document.getElementById('date').onchange = (e) => {         
+    let curDate = e.currentTarget.value.replaceAll('-', '');    
+    newDate(curDate);
+        
+    document.getElementById('search').value = '';
+}
+
+newDate = (date) => {
+    fetch(`https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=${date}&json`)
     .then((data) => {
         return data.json();
     }).then((data) => {
         rates = data.map((rate) => {
             return {
-                r030: rate.r030,
-                txt: rate.txt,
-                rate: rate.rate,
+                name: rate.txt,
+                rate: rate.rate.toFixed(2),
                 cc: rate.cc,
-                exchangedate: rate.exchangedate
             }
         });
     renderRates(rates);
     });
-    document.getElementById('search').value = '';
 }
-
-fetch(`https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=${currentDate(new Date())}&json`)
-.then((data) => {
-    return data.json();
-}).then((data) => {
-    rates = data.map((rate) => {
-        return {
-            r030: rate.r030,
-            txt: rate.txt,
-            rate: rate.rate,
-            cc: rate.cc,
-            exchangedate: rate.exchangedate
-        }
-    });
-    renderRates(rates);        
-});
-
